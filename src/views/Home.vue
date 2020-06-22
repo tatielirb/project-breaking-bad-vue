903998909604b1655216ab706b02f88d287facad
Tatieli Ramos <tatielirb@gmail.com>
2020-06-21 21:57:17 -0300

    Adustment aplication vue with typescript and add bootstrap

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
 
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
+  @Component
+  export default class Home extends Vue {}
 </script>
+
+<style lang="less" scoped>
+  @import './../assets/index.less';
+</style>
